8e30e020165e27434a0b6fb8eb2708e645d0a4a4
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-01-28 14:08:59 -0500

    modified notebook log

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ class HistoryWindow(Screen):
             item = OneLineListItem(text=entry[2])
             setattr(item, 'entry_id', entry[0])
             item.bind(on_press=lambda x, item=item: self.confirm_delete(item))
+            item.font_name = 'contentfont3'
             item.theme_text_color = "Custom"
             item.text_color: (0.3, 0.6, 0.6, 1)  
             self.entries.add_widget(item)
